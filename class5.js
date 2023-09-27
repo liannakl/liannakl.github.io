05e65b8c80aca9db24896c8045438c9216eea056
liannakl <lianna.ling93@myhunter.cuny.edu>
2023-09-27 00:07:29 -0400

    create class5

---
+++ class5.js
@@ -0,0 +1,31 @@
+function getInputValue(){
+    // Selecting the input element and get its value 
+    var inputVal = document.getElementById("myInput").value;
+    //Displaying the Value on the page
+    document.getElementById("display").innerHTML = "<br>" + inputVal ;
+
+var driver = inputVal;
+
+if (driver === 'Max Verstappen') {
+console.log('Two time Formula 1 World Champion with Red Bull Racing');
+document.getElementById("seasonal").innerHTML= "<img src='https://media.formula1.com/content/dam/fom-website/drivers/2023Drivers/verstappen.jpg.img.640.medium.jpg/1677069646195.jpg'>";
+
+}else if(season === 'winter'){
+document.getElementById("seasonal").innerHTML= "<img src='winter.jpg'>";
+console.log('It\'s winter! Everything is covered in snow.');      
+ } 
+else if(season === 'fall'){
+document.getElementById("seasonal").innerHTML= "<img src='fall.jpg'>";
+console.log('It\'s fall! Leaves are falling!');      
+ } 
+else if(season === 'summer'){
+document.getElementById("seasonal").innerHTML= "<img src='summer.jpg'>";
+console.log('It\'s sunny and warm because it\'s summer!');      
+ } 
+else {
+console.log('Invalid season.');
+document.getElementById("seasonal").innerHTML= "invalid season";
+
+}
+}
+
